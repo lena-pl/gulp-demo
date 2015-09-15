7fcbe44f2d3edb8a8f4dc7d524b6b64937ef621a
Lena Plaksina <lena.plaksina@gmail.com>
2015-09-16 11:25:57 +1200

    Add jshint, uglify, js-build and error messages

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,64 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var notify = require("gulp-notify");
+var notify = require('gulp-notify');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
-  return gulp.src("scss/main.scss")
-    .pipe(notify("Got a file!"));
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
+
+// gulp.task('default', ['build-css']);
+
+// configure the scss task
+gulp.task('build-css', function() {
+  return gulp.src("scss/**/*.scss")
+    .pipe(sourcemaps.init())
+      .pipe(sass()
+        .on('error', function(err) {
+          notify({title: "Sass Error"}).write(err);
+          this.emit('end');
+        })
+      )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css'))
+    .pipe(notify("Css compiled!"));
+});
+
+// configure the jshint task
+gulp.task('jshint', function() {
+  return gulp.src('scripts/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(notify("JS checked!"));
+});
+
+// configure the js concat task
+gulp.task('build-js', function() {
+  return gulp.src('scripts/**/*.js')
+    .pipe(sourcemaps.init())
+      //only uglify if gulp is ran with '--type production'
+      .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+      .pipe(concat('app.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('js'))
+    .pipe(notify("JS files compiled!"));
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+  gulp.watch(['scss/**/*.scss'], ['build-css'])
+    .on('change', function(event) {
+      gutil.log('File ' + gutil.colors.magenta(event.path) + ' was ' + event.type + '...');
+    });
+  gulp.watch(['scripts/**/*.js'], ['jshint'])
+    .on('change', function(event) {
+      gutil.log('File ' + gutil.colors.green(event.path) + ' was ' + event.type + '...');
+    });
+  gulp.watch(['scripts/**/*.js'], ['build-js'])
+    .on('change', function(event) {
+      gutil.log('File ' + gutil.colors.black(event.path) + ' was ' + event.type + '...');
+    });
 });
